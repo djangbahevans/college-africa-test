d80536142b71a803a6c163ba0378545546fae976
Evans Djangbah <djangbahevans@yahoo.com>
2023-12-18 23:14:26 +0000

    Add dependencies and configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  extends: [
+    'universe/native',
+    'universe/shared/typescript-analysis',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        // '@typescript-eslint/no-unsafe-assignment': 'off',
+        // '@typescript-eslint/no-unsafe-call': 'off',
+        // '@typescript-eslint/no-unsafe-member-access': 'off'
+      },
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    }
+  ]
+};
